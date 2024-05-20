0ef5c5581d27413fd879fe9c3e2c8a8fb9cb28b2
Abdulaziz22022010 <163529940+Abdulaziz22022010@users.noreply.github.com>
2024-05-20 16:01:52 +0500

    Add files via upload

---
+++ PortfolioN3/app.js
@@ -0,0 +1,145 @@
+const mebellar = [
+  {
+    id: "1",
+    url: "https://cdn0.divan.by/img/v1/VU6xRGz4h8-3X6Rkt2w7Kpc3D5mkdhaExfK3Ef0OVI4/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4MzcyMTIucG5n.jpg",
+    title:'Сан-Паулу Velvet Brown',
+    text:'Барные стулья',
+    text2:'4 690₽',
+    
+  },
+  {
+    id: "2",
+    url: "https://cdn0.divan.by/img/v1/MosgNqkqsa5gr23QcTsxSy9wUomYQYbbF-d3GbPBYis/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:95/czM6Ly9kaXZhbi9wcm9kdWN0LzQzOTc5MDcucG5n.jpg",
+    title:'Валенсия Beige',
+    text:'стол',
+    text2:'28 490₽',
+    
+  },
+  {
+    id: "3",
+    url: "https://cdn0.divan.by/img/v1/kCwpkKRbWoViiDDcgLMySpWnu3MyQ27_wgvjX7hGFwE/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzUwMTM1NjUucG5n.jpg",
+    title:'Барные стулья',
+    text:'Диваны',
+    text2:'1 200₽', 
+  },
+  {
+    id: "4",
+    url: "https://cdn0.divan.by/img/v1/IGXfF0-XWBTtLIwfhKTohVmgLavjAELHD-U0uYTtPC4/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4MTU0OTcucG5n.jpg",
+    title:'Шерона Barhat Grey',
+    text:'Диваны',
+    text2:'8 490₽',
+    
+  },
+  {
+    id: "5",
+    url: "https://cdn0.divan.by/img/v1/7CwFcpOG0V6R6lqdo1zIDmLRjHD9Q4lQ1t2Q7GP4Czw/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ0NDI0ODEucG5n.jpg",
+    title:'Сан-Паулу Velvet Brown',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+  {
+    id: "6",
+    url: "https://cdn0.divan.by/img/v1/ApA1Tq1q4JNdH1pBjo6zWf-Adz-BLg1C72eWCAvS0aI/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ3NDc5NTIucG5n.jpg",
+    title:'Валенсия Beige',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+
+  {
+    id: "7",
+    url: "https://cdn0.divan.by/img/v1/ba1XDvFcy1HVW_GPz3mqwSR-FZebtrtMSKS7Op0i0_E/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4Nzk5NTAucG5n.jpg",
+    title:'Авиньон-1',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+  {
+    id: "8",
+    url: "https://cdn0.divan.by/img/v1/VsFlZhpvnNBqQes6AWOG1iN11kDLQrNeecJyWYPabfk/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ5MDA3NjYucG5n.jpg",
+    title:'Толикс-2 White Gloss',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+  {
+    id: "1",
+    url: "https://cdn0.divan.by/img/v1/VU6xRGz4h8-3X6Rkt2w7Kpc3D5mkdhaExfK3Ef0OVI4/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4MzcyMTIucG5n.jpg",
+    title:'Сан-Паулу Velvet Brown',
+    text:'Барные стулья',
+    text2:'4 690₽',
+    
+  },
+  {
+    id: "2",
+    url: "https://cdn0.divan.by/img/v1/MosgNqkqsa5gr23QcTsxSy9wUomYQYbbF-d3GbPBYis/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:95/czM6Ly9kaXZhbi9wcm9kdWN0LzQzOTc5MDcucG5n.jpg",
+    title:'Валенсия Beige',
+    text:'стол',
+    text2:'28 490₽',
+    
+  },
+  {
+    id: "3",
+    url: "https://cdn0.divan.by/img/v1/kCwpkKRbWoViiDDcgLMySpWnu3MyQ27_wgvjX7hGFwE/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzUwMTM1NjUucG5n.jpg",
+    title:'Барные стулья',
+    text:'Диваны',
+    text2:'1 200₽',
+  },
+  {
+    id: "4",
+    url: "https://cdn0.divan.by/img/v1/IGXfF0-XWBTtLIwfhKTohVmgLavjAELHD-U0uYTtPC4/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4MTU0OTcucG5n.jpg",
+    title:'Шерона Barhat Grey',
+    text:'Диваны',
+    text2:'8 490₽',
+    
+  },
+  {
+    id: "5",
+    url: "https://cdn0.divan.by/img/v1/7CwFcpOG0V6R6lqdo1zIDmLRjHD9Q4lQ1t2Q7GP4Czw/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ0NDI0ODEucG5n.jpg",
+    title:'Сан-Паулу Velvet Brown',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+  {
+    id: "6",
+    url: "https://cdn0.divan.by/img/v1/ApA1Tq1q4JNdH1pBjo6zWf-Adz-BLg1C72eWCAvS0aI/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ3NDc5NTIucG5n.jpg",
+    title:'Валенсия Beige',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+
+  {
+    id: "7",
+    url: "https://cdn0.divan.by/img/v1/ba1XDvFcy1HVW_GPz3mqwSR-FZebtrtMSKS7Op0i0_E/t:0::0:0/pd:60:60:60:60/rs:fit:1148:720:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ4Nzk5NTAucG5n.jpg",
+    title:'Авиньон-1',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+
+  {
+    id: "8",
+    url: "https://cdn0.divan.by/img/v1/VsFlZhpvnNBqQes6AWOG1iN11kDLQrNeecJyWYPabfk/t:0::0:0/pd:123:60:123:60/rs:fit:1148:594:0:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQ5MDA3NjYucG5n.jpg",
+    title:'Толикс-2 White Gloss',
+    text:'Диваны',
+    text2:'28 490₽',
+  },
+];
+
+let row =document.querySelector('.row')
+
+let html='';
+  
+
+mebellar.forEach((item)=>{
+   html+=`<div class="card">
+   <img src="${item.url}" class="card-img-top" alt="..." />
+   <div class="card-body">
+     <h5 class="card-title">${item.title}</h5>
+     <p class="card-text">
+     <br>
+       ${item.text}
+     </p>
+     <h5>${item.text2}</h5>
+     <br>
+     <button class="btn">Go somewhere></button>
+   </div>
+ </div>`
+});
+row.innerHTML=html
